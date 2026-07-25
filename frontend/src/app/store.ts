1b76473f3d27b492e6e3f98f9cf771d01ddab116
santoshabothula <118504847+santoshabothula@users.noreply.github.com>
2026-07-24 21:59:17 -0500

    Add files via upload

---
+++ frontend/src/app/store.ts
@@ -0,0 +1,1 @@
+import {configureStore} from '@reduxjs/toolkit'; import metadata from '../features/metadata/slice'; export const store=configureStore({reducer:{metadata}}); export type RootState=ReturnType<typeof store.getState>; export type AppDispatch=typeof store.dispatch;
